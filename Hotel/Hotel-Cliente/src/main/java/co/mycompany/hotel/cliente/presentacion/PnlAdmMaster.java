d6dffa82d5a66beac73437d4573d076936069549
kevindm9 <74603789+kevindm9@users.noreply.github.com>
2022-03-03 19:55:30 -0500

    adicionar Hotel Master

--- Hotel/Hotel-Cliente/src/main/java/co/mycompany/hotel/cliente/presentacion/PnlAdmMaster.java
+++ Hotel/Hotel-Cliente/src/main/java/co/mycompany/hotel/cliente/presentacion/PnlAdmMaster.java
@@ -34,6 +34,7 @@ public class PnlAdmMaster extends javax.swing.JPanel {
     private void initComponents() {
 
         GrupoHab = new javax.swing.ButtonGroup();
+        buttonGroup1 = new javax.swing.ButtonGroup();
         pnlAdmPrincipal = new javax.swing.JPanel();
         tbpMadMenu = new javax.swing.JTabbedPane();
         pnlAdmHabitaciones = new javax.swing.JPanel();
@@ -44,12 +45,12 @@ public class PnlAdmMaster extends javax.swing.JPanel {
         bgAdmHabConsultar = new javax.swing.JRadioButton();
         pnlAdmHabControl = new javax.swing.JPanel();
         pnlAdmSucursales = new javax.swing.JPanel();
-        pnlAdmHabMain1 = new javax.swing.JPanel();
-        bgAdmHabCrear1 = new javax.swing.JRadioButton();
-        bgAdmHabModificar1 = new javax.swing.JRadioButton();
-        bgAdmHabEliminar1 = new javax.swing.JRadioButton();
-        jRadioButton2 = new javax.swing.JRadioButton();
         pnlAdmHabControl1 = new javax.swing.JPanel();
+        pnlAdmHabMain4 = new javax.swing.JPanel();
+        bgAdmHotCrear = new javax.swing.JRadioButton();
+        bgAdmHotModificar = new javax.swing.JRadioButton();
+        bgAdmHotEliminar = new javax.swing.JRadioButton();
+        bgAdmHotCons = new javax.swing.JRadioButton();
         pnlAdmHabitaciones1 = new javax.swing.JPanel();
         pnlAdmHabMain2 = new javax.swing.JPanel();
         bgAdmHabCrear2 = new javax.swing.JRadioButton();
@@ -71,11 +72,6 @@ public class PnlAdmMaster extends javax.swing.JPanel {
                 bgAdmHabCrearStateChanged(evt);
             }
         });
-        bgAdmHabCrear.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                bgAdmHabCrearMouseClicked(evt);
-            }
-        });
 
         GrupoHab.add(bgAdmHabModificar);
         bgAdmHabModificar.setText("Modificar");
@@ -93,11 +89,6 @@ public class PnlAdmMaster extends javax.swing.JPanel {
         bgAdmHabConsultar.addChangeListener(new javax.swing.event.ChangeListener() {
             public void stateChanged(javax.swing.event.ChangeEvent evt) {
                 bgAdmHabConsultarStateChanged(evt);
-            }
-        });
-        bgAdmHabConsultar.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                bgAdmHabConsultarMouseClicked(evt);
             }
         });
 
@@ -151,70 +142,81 @@ public class PnlAdmMaster extends javax.swing.JPanel {
 
         tbpMadMenu.addTab("Habitaciones", pnlAdmHabitaciones);
 
-        pnlAdmHabMain1.setBorder(javax.swing.BorderFactory.createTitledBorder("Procesos"));
+        pnlAdmHabControl1.setRequestFocusEnabled(false);
+        pnlAdmHabControl1.setLayout(new java.awt.CardLayout());
 
-        GrupoHab.add(bgAdmHabCrear1);
-        bgAdmHabCrear1.setText("Crear");
-        bgAdmHabCrear1.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                bgAdmHabCrear1MouseClicked(evt);
+        pnlAdmHabMain4.setBorder(javax.swing.BorderFactory.createTitledBorder("Procesos"));
+
+        GrupoHab.add(bgAdmHotCrear);
+        bgAdmHotCrear.setText("Crear");
+        bgAdmHotCrear.addChangeListener(new javax.swing.event.ChangeListener() {
+            public void stateChanged(javax.swing.event.ChangeEvent evt) {
+                bgAdmHotCrearStateChanged(evt);
             }
         });
 
-        GrupoHab.add(bgAdmHabModificar1);
-        bgAdmHabModificar1.setText("Modificar");
+        GrupoHab.add(bgAdmHotModificar);
+        bgAdmHotModificar.setText("Modificar");
+        bgAdmHotModificar.addChangeListener(new javax.swing.event.ChangeListener() {
+            public void stateChanged(javax.swing.event.ChangeEvent evt) {
+                bgAdmHotModificarStateChanged(evt);
+            }
+        });
 
-        GrupoHab.add(bgAdmHabEliminar1);
-        bgAdmHabEliminar1.setText("Eliminar");
+        GrupoHab.add(bgAdmHotEliminar);
+        bgAdmHotEliminar.setText("Eliminar");
 
-        jRadioButton2.setText("Consutar");
+        GrupoHab.add(bgAdmHotCons);
+        bgAdmHotCons.setText("Consutar");
+        bgAdmHotCons.addChangeListener(new javax.swing.event.ChangeListener() {
+            public void stateChanged(javax.swing.event.ChangeEvent evt) {
+                bgAdmHotConsStateChanged(evt);
+            }
+        });
 
-        javax.swing.GroupLayout pnlAdmHabMain1Layout = new javax.swing.GroupLayout(pnlAdmHabMain1);
-        pnlAdmHabMain1.setLayout(pnlAdmHabMain1Layout);
-        pnlAdmHabMain1Layout.setHorizontalGroup(
-            pnlAdmHabMain1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(pnlAdmHabMain1Layout.createSequentialGroup()
+        javax.swing.GroupLayout pnlAdmHabMain4Layout = new javax.swing.GroupLayout(pnlAdmHabMain4);
+        pnlAdmHabMain4.setLayout(pnlAdmHabMain4Layout);
+        pnlAdmHabMain4Layout.setHorizontalGroup(
+            pnlAdmHabMain4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pnlAdmHabMain4Layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(pnlAdmHabMain1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(bgAdmHabEliminar1)
-                    .addComponent(bgAdmHabModificar1)
-                    .addComponent(bgAdmHabCrear1)
-                    .addComponent(jRadioButton2))
+                .addGroup(pnlAdmHabMain4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(bgAdmHotEliminar)
+                    .addComponent(bgAdmHotModificar)
+                    .addComponent(bgAdmHotCrear)
+                    .addComponent(bgAdmHotCons))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
-        pnlAdmHabMain1Layout.setVerticalGroup(
-            pnlAdmHabMain1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(pnlAdmHabMain1Layout.createSequentialGroup()
-                .addComponent(bgAdmHabCrear1)
+        pnlAdmHabMain4Layout.setVerticalGroup(
+            pnlAdmHabMain4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pnlAdmHabMain4Layout.createSequentialGroup()
+                .addComponent(bgAdmHotCrear)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jRadioButton2)
+                .addComponent(bgAdmHotCons)
                 .addGap(5, 5, 5)
-                .addComponent(bgAdmHabModificar1)
+                .addComponent(bgAdmHotModificar)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(bgAdmHabEliminar1)
+                .addComponent(bgAdmHotEliminar)
                 .addContainerGap(17, Short.MAX_VALUE))
         );
-
-        pnlAdmHabControl1.setRequestFocusEnabled(false);
-        pnlAdmHabControl1.setLayout(new java.awt.CardLayout());
 
         javax.swing.GroupLayout pnlAdmSucursalesLayout = new javax.swing.GroupLayout(pnlAdmSucursales);
         pnlAdmSucursales.setLayout(pnlAdmSucursalesLayout);
         pnlAdmSucursalesLayout.setHorizontalGroup(
             pnlAdmSucursalesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(pnlAdmSucursalesLayout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(pnlAdmHabMain1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(pnlAdmHabMain4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(pnlAdmHabControl1, javax.swing.GroupLayout.PREFERRED_SIZE, 570, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
         pnlAdmSucursalesLayout.setVerticalGroup(
             pnlAdmSucursalesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(pnlAdmHabControl1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(pnlAdmHabControl1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 237, Short.MAX_VALUE)
             .addGroup(pnlAdmSucursalesLayout.createSequentialGroup()
                 .addGap(36, 36, 36)
-                .addComponent(pnlAdmHabMain1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(59, Short.MAX_VALUE))
+                .addComponent(pnlAdmHabMain4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         tbpMadMenu.addTab("Sucursales", pnlAdmSucursales);
@@ -226,11 +228,6 @@ public class PnlAdmMaster extends javax.swing.JPanel {
         bgAdmHabCrear2.addChangeListener(new javax.swing.event.ChangeListener() {
             public void stateChanged(javax.swing.event.ChangeEvent evt) {
                 bgAdmHabCrear2StateChanged(evt);
-            }
-        });
-        bgAdmHabCrear2.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                bgAdmHabCrear2MouseClicked(evt);
             }
         });
 
@@ -245,11 +242,6 @@ public class PnlAdmMaster extends javax.swing.JPanel {
         bgAdmHabConsultar1.addChangeListener(new javax.swing.event.ChangeListener() {
             public void stateChanged(javax.swing.event.ChangeEvent evt) {
                 bgAdmHabConsultar1StateChanged(evt);
-            }
-        });
-        bgAdmHabConsultar1.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                bgAdmHabConsultar1MouseClicked(evt);
             }
         });
 
@@ -298,7 +290,7 @@ public class PnlAdmMaster extends javax.swing.JPanel {
             .addGroup(pnlAdmHabitaciones1Layout.createSequentialGroup()
                 .addGap(36, 36, 36)
                 .addComponent(pnlAdmHabMain2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(59, Short.MAX_VALUE))
+                .addContainerGap(58, Short.MAX_VALUE))
         );
 
         tbpMadMenu.addTab("Administradores Junior", pnlAdmHabitaciones1);
@@ -341,31 +333,13 @@ public class PnlAdmMaster extends javax.swing.JPanel {
         );
     }// </editor-fold>//GEN-END:initComponents
 
-    private void bgAdmHabConsultar1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_bgAdmHabConsultar1MouseClicked
-        // TODO add your handling code here:
-    }//GEN-LAST:event_bgAdmHabConsultar1MouseClicked
-
     private void bgAdmHabConsultar1StateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_bgAdmHabConsultar1StateChanged
         // TODO add your handling code here:
     }//GEN-LAST:event_bgAdmHabConsultar1StateChanged
 
-    private void bgAdmHabCrear2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_bgAdmHabCrear2MouseClicked
-        // TODO add your handling code here:
-    }//GEN-LAST:event_bgAdmHabCrear2MouseClicked
-
     private void bgAdmHabCrear2StateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_bgAdmHabCrear2StateChanged
         // TODO add your handling code here:
     }//GEN-LAST:event_bgAdmHabCrear2StateChanged
-
-    private void bgAdmHabCrear1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_bgAdmHabCrear1MouseClicked
-        // TODO add your handling code here:
-
-    }//GEN-LAST:event_bgAdmHabCrear1MouseClicked
-
-    private void bgAdmHabConsultarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_bgAdmHabConsultarMouseClicked
-        // TODO add your handling code here:
-
-    }//GEN-LAST:event_bgAdmHabConsultarMouseClicked
 
     private void bgAdmHabConsultarStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_bgAdmHabConsultarStateChanged
         // TODO add your handling code here:
@@ -377,11 +351,6 @@ public class PnlAdmMaster extends javax.swing.JPanel {
             pnlAdmHabControl.revalidate();
         }
     }//GEN-LAST:event_bgAdmHabConsultarStateChanged
-
-    private void bgAdmHabCrearMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_bgAdmHabCrearMouseClicked
-        // TODO add your handling code here:
-
-    }//GEN-LAST:event_bgAdmHabCrearMouseClicked
 
     private void bgAdmHabCrearStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_bgAdmHabCrearStateChanged
         // TODO add your handling code here:
@@ -396,7 +365,7 @@ public class PnlAdmMaster extends javax.swing.JPanel {
 
     private void bgAdmHabModificarStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_bgAdmHabModificarStateChanged
         // TODO add your handling code here:
-            if (bgAdmHabModificar.isSelected()) {
+        if (bgAdmHabModificar.isSelected()) {
             PnlAdmHabModificar panelHabAdd = new PnlAdmHabModificar("All_Hotels");
             pnlAdmHabControl.removeAll();
             pnlAdmHabControl.add(panelHabAdd);
@@ -405,28 +374,60 @@ public class PnlAdmMaster extends javax.swing.JPanel {
         }
     }//GEN-LAST:event_bgAdmHabModificarStateChanged
 
+    private void bgAdmHotCrearStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_bgAdmHotCrearStateChanged
+        // TODO add your handling code here:
+        if (bgAdmHotCrear.isSelected()) {
+            PnlAdmHotelAdd panelHotAdd = new PnlAdmHotelAdd("All_Hotels");
+            pnlAdmHabControl1.removeAll();
+            pnlAdmHabControl1.add(panelHotAdd);
+            pnlAdmHabControl1.repaint();
+            pnlAdmHabControl1.revalidate();
+        }
+    }//GEN-LAST:event_bgAdmHotCrearStateChanged
+
+    private void bgAdmHotModificarStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_bgAdmHotModificarStateChanged
+        // TODO add your handling code here:
+    }//GEN-LAST:event_bgAdmHotModificarStateChanged
+
+    private void bgAdmHotConsStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_bgAdmHotConsStateChanged
+        // TODO add your handling code here:
+            if (bgAdmHotCons.isSelected()) {
+                PnlUsuSucursales panelHot = new PnlUsuSucursales(panel);
+            pnlAdmHabControl1.removeAll();
+            pnlAdmHabControl1.add(panelHot);
+            pnlAdmHabControl1.repaint();
+            pnlAdmHabControl1.revalidate();
+        }
+    }//GEN-LAST:event_bgAdmHotConsStateChanged
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.ButtonGroup GrupoHab;
     private javax.swing.JRadioButton bgAdmHabConsultar;
     private javax.swing.JRadioButton bgAdmHabConsultar1;
+    private javax.swing.JRadioButton bgAdmHabConsultar2;
     private javax.swing.JRadioButton bgAdmHabCrear;
     private javax.swing.JRadioButton bgAdmHabCrear1;
     private javax.swing.JRadioButton bgAdmHabCrear2;
     private javax.swing.JRadioButton bgAdmHabEliminar;
-    private javax.swing.JRadioButton bgAdmHabEliminar1;
     private javax.swing.JRadioButton bgAdmHabEliminar2;
+    private javax.swing.JRadioButton bgAdmHabEliminar3;
     private javax.swing.JRadioButton bgAdmHabModificar;
-    private javax.swing.JRadioButton bgAdmHabModificar1;
     private javax.swing.JRadioButton bgAdmHabModificar2;
+    private javax.swing.JRadioButton bgAdmHabModificar3;
+    private javax.swing.JRadioButton bgAdmHotCons;
+    private javax.swing.JRadioButton bgAdmHotCrear;
+    private javax.swing.JRadioButton bgAdmHotEliminar;
+    private javax.swing.JRadioButton bgAdmHotModificar;
+    private javax.swing.ButtonGroup buttonGroup1;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JRadioButton jRadioButton2;
     private javax.swing.JPanel pnlAdmHabControl;
     private javax.swing.JPanel pnlAdmHabControl1;
     private javax.swing.JPanel pnlAdmHabControl2;
     private javax.swing.JPanel pnlAdmHabMain;
-    private javax.swing.JPanel pnlAdmHabMain1;
     private javax.swing.JPanel pnlAdmHabMain2;
+    private javax.swing.JPanel pnlAdmHabMain3;
+    private javax.swing.JPanel pnlAdmHabMain4;
     private javax.swing.JPanel pnlAdmHabitaciones;
     private javax.swing.JPanel pnlAdmHabitaciones1;
     private javax.swing.JPanel pnlAdmPrincipal;
